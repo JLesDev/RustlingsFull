8e28313de8024c9e7729e4e671ffa7e73356a7bf
Jonty Leslie <jontyleslie@gmail.com>
2024-11-20 00:51:08 +0000

    no update

--- exercises/09_strings/strings4.rs
+++ exercises/09_strings/strings4.rs
@@ -1,5 +1,7 @@
 // Calls of this function should be replaced with calls of `string_slice` or `string`.
-fn placeholder() {}
+fn placeholder() {
+    
+}
 
 fn string_slice(arg: &str) {
     println!("{arg}");
