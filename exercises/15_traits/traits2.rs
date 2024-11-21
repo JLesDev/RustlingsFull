ab24c39ff4d332819336206902bd1ec9864bad41
Jonty Leslie <jontyleslie@gmail.com>
2024-11-21 06:07:06 +0000

    15-02 progress

--- exercises/15_traits/traits2.rs
+++ exercises/15_traits/traits2.rs
@@ -4,20 +4,34 @@ trait AppendBar {
 
 // TODO: Implement the trait `AppendBar` for a vector of strings.
 // `append_bar` should push the string "Bar" into the vector.
-
+/* 
 impl AppendBar for Vec<String> {
     
     fn append_bar(self)-> Vec<String>{
         let bara = String::from("Bar");
         //let append_bara = new Vec::<String>;
-        let v = vec![""];
+        let v = vec!["asfd"];
         v.push(&bara.to_string())
-        
-        
+
+
+    }
+
+
+}*/
+
+
+impl AppendBar for Vec<String> {
+    // TODO: Implement `AppendBar` for the type `String`.
+    //append_bar("Bar".to_owned(self));
+    fn append_bar(self)-> Self{
+        let bara = String::from("Bar");
+        let mut v = vec![String::from("Hi")];
+        self.push(bara)
     }
 
 
 }
+
 
 fn main() {
     // You can optionally experiment here.
