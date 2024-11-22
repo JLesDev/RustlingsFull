becb5dcd1021c1615d0bf6cc523ffb6872c1196e
Jonty Leslie <jontyleslie@gmail.com>
2024-11-22 05:50:33 +0000

    22

--- exercises/22_clippy/clippy2.rs
+++ exercises/22_clippy/clippy2.rs
@@ -2,9 +2,14 @@ fn main() {
     let mut res = 42;
     let option = Some(12);
     // TODO: Fix the Clippy lint.
-    for x in option {
+
+    if let x < option{
         res += x;
+        x += 1;
     }
+    /*for x in option {
+        res += x;
+    }*/
 
     println!("{res}");
 }
