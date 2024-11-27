ada6a9189f09a1cf1cb56696b517e5c4ba0ce697
Jonty Leslie <jontyleslie@gmail.com>
2024-11-27 22:34:19 +0000

    updating some local files into git

--- exercises/20_threads/threads1.rs
+++ exercises/20_threads/threads1.rs
@@ -1,37 +1,28 @@
-// This program spawns multiple threads that each runs for at least 250ms, and
-// each thread returns how much time it took to complete. The program should
-// wait until all the spawned threads have finished and should collect their
-// return values into a vector.
+// Here are some more easy Clippy fixes so you can see its utility 📎
+// TODO: Fix all the Clippy lints.
 
-use std::{
-    thread,
-    time::{Duration, Instant},
-};
-
+#[rustfmt::skip]
+#[allow(unused_variables, unused_assignments)]
 fn main() {
-    let mut handles = Vec::new();
-    for i in 0..10 {
-        let handle = thread::spawn(move || {
-            let start = Instant::now();
-            thread::sleep(Duration::from_millis(250));
-            println!("Thread {i} done");
-            start.elapsed().as_millis()
-        });
-        handles.push(handle);
+    let my_option: Option<()> = None;
+    if my_option.is_none() {
+        println!("{:?}", my_option);
     }
 
-    let mut results = Vec::new();
-    for handle in handles {
-        // TODO: Collect the results of all threads into the `results` vector.
-        // Use the `JoinHandle` struct which is returned by `thread::spawn`.
-    }
+    let my_arr = &[
+        -1, -2, -3,
+        -4, -5, -6
+    ];
+    println!("My array! Here it is: {my_arr:?}");
 
-    if results.len() != 10 {
-        panic!("Oh no! Some thread isn't done yet!");
-    }
+    let my_empty_vec = vec![1, 2, 3, 4, 5].resize(0, 5);
+    println!("This Vec is empty, see? {my_empty_vec:?}");
 
-    println!();
-    for (i, result) in results.into_iter().enumerate() {
-        println!("Thread {i} took {result}ms");
-    }
+    let mut value_a = 45;
+    let mut value_b = 66;
+    // Let's swap these two!
+    std::mem::swap(&mut value_a, &mut value_b);
+    // value_a = value_b;
+    // value_b = value_a;
+    println!("value a: {value_a}; value b: {value_b}");
 }
