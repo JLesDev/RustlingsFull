eb39ec7c189ae4048e1e06c6a6b64b4d86eddfdd
Jonty Leslie <jontyleslie@gmail.com>
2024-12-13 06:01:19 +0000

    updating some local files into git

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -34,7 +34,14 @@ impl Default for Person {
 // 5. Parse the second element from the split operation into a `u8` as the age.
 // 6. If parsing the age fails, return the default of `Person`.
 impl From<&str> for Person {
-    fn from(s: &str) -> Self {}
+    fn from(s: &str) -> Self {
+
+        if let mut split = s.split(','){
+            Ok(Self)
+        }
+
+        Err()
+    }
 }
 
 fn main() {
